04ef9c982897647eea9729b79225d4d956f236a4
mustafashifters <mustafa.ahmed@shifters.tech>
2020-05-07 15:17:07 +0200

    add home page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Navbar></Navbar>
     <router-view />
+    <Footer></Footer>
   </div>
 </template>
-
+<script>
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+/* @import "./assets/vendor/bootstrap/css/bootstrap.min.css"; */
+@import "./assets/vendor/icofont/icofont.min.css";
+@import "./assets/vendor/boxicons/css/boxicons.min.css";
+@import "./assets/vendor/animate.css/animate.min.css";
+@import "./assets/vendor/venobox/venobox.css";
+@import "./assets/vendor/owl.carousel/assets/owl.carousel.min.css";
+@import "./assets/vendor/aos/aos.css";
+@import "./assets/css/style.css";
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,26 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import $ from "jquery";
+import jQuery from "jquery";
+window.jQuery = window.$ = jQuery;
+import AOS from "aos";
+window.AOS = AOS;
+// require("./assets/vendor/jquery/jquery.min.js");
+// require("./assets/vendor/bootstrap/js/bootstrap.bundle.min.js");
+require("./assets/vendor/jquery.easing/jquery.easing.min.js");
+require("./assets/vendor/php-email-form/validate.js");
+require("./assets/vendor/jquery-sticky/jquery.sticky.js");
+require("./assets/vendor/isotope-layout/isotope.pkgd.js");
+require("./assets/vendor/venobox/venobox.js");
+require("./assets/vendor/waypoints/jquery.waypoints.min.js");
+require("./assets/vendor/owl.carousel/owl.carousel.js");
+// require("./assets/vendor/aos/aos.js");
+require("./assets/js/main.js");
 
 Vue.config.productionTip = false;
-
 new Vue({
   router,
   store,
